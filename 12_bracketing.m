a6ef66207630f03b3e7344c46f138aa6599e1972
KhushalDurand <ikhushaldurand@gmail.com>
2026-03-12 16:44:06 +0530

    Implement bisection and false position methods
    Added bisection and false position methods for root finding.

--- 12_bracketing.m
+++ 12_bracketing.m
@@ -14,7 +14,11 @@ if f(a)*f(b) > 0
     disp('Invalid interval. f(a) and f(b) must have opposite signs.');
 else
     for i = 1:max_iter
+        1. bisection
         c = (a + b)/2;  
+        2. false position
+        m = (f(xu)-f(xl))/(xu-xl);
+        c = xl - f(xl)/m;
         
         if abs(f(c)) < tol
             break;
@@ -30,3 +34,6 @@ else
     fprintf('Root = %.6f\n', c);
     fprintf('Iterations = %d\n', i);
 end
+
+
+
