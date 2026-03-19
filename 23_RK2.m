da3a4e4758e5b3ae6a127fd89b16c45b3783ef89
KhushalDurand <ikhushaldurand@gmail.com>
2026-03-20 00:15:35 +0530

    Create 23_RK2.m

---
+++ 23_RK2.m
@@ -0,0 +1,33 @@
+% RK2 Mid point method
+
+clc;
+clear;
+
+function dydx = ode_system(x, y)
+    dy1dx = y(2);
+    dy2dx = -y(1);
+    dydx = [dy1dx; dy2dx]; 
+end
+
+h = 0.1;
+n = 50;
+
+x = 0:h:5;
+y = zeros(2,n+1);
+
+y(1,1) = 0;
+y(2,1) = 1;
+
+for i = 1:n
+    k1 = ode_system(x(i), y(:,i));
+    k2 = ode_system(x(i) + h/2, y(:,i) + (h/2)*k1);
+    
+    y(:,i+1) = y(:,i) + h * k2;
+end
+
+plot(x, y(1,:), '-o', x, y(2,:), '-x');
+legend('y1','y2');
+xlabel('x');
+ylabel('Solution');
+title('RK2 Midpoint Method');
+grid on;
