4c491dc90f4590272d51ee4bfaf3cee78e9c708c
KhushalDurand <ikhushaldurand@gmail.com>
2026-03-20 00:21:15 +0530

    Create 25_RK4.m

---
+++ 25_RK4.m
@@ -0,0 +1,35 @@
+% RK3 method
+
+clc;
+clear;
+
+function dydx = ode_system(x, y)
+    dy1dx = y(2);
+    dy2dx = -y(1);
+    dydx = [dy1dx; dy2dx]; 
+end
+
+h = 0.1;
+n = 50;
+
+x = 0:h:5;
+y = zeros(2,n+1);
+
+y(1,1) = 0;
+y(2,1) = 1;
+
+for i = 1:n
+    k1 = ode_system(x(i), y(:,i));
+    k2 = ode_system(x(i) + h/2, y(:,i) + (h/2)*k1);
+    k3 = ode_system(x(i)+h/2,y(:,i)+k2*h/2);
+    k4 = ode_system(x(i)+h,y(:,i)+k3*h);
+    
+    y(:,i+1) = y(:,i) + (h/6)*(k1+2*k2+2*k3+k4);
+end
+
+plot(x, y(1,:), '-o', x, y(2,:), '-x');
+legend('y1','y2');
+xlabel('x');
+ylabel('Solution');
+title('RK4 Method');
+grid on;
